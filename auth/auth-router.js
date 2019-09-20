a235caccb45933f403b6de3bbbbd392b4d4d091a
Desiree Morris <desiquinncompany@gmail.com>
2019-09-20 11:56:07 -0400

    added a user model

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,11 +3,15 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  
 });
 
 module.exports = router;
